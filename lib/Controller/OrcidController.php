543af1747d630edf7d01da8749213ae61b5737e6
daita <maxence@pontapreta.net>
2017-02-07 22:30:35 -0100

    retreive oauth token

--- lib/Controller/OrcidController.php
+++ lib/Controller/OrcidController.php
@@ -37,84 +37,78 @@ class OrcidController extends Controller
 
     private $configService;
 
-    private $userId;
-
     private $miscService;
 
-    public function __construct($appName, IRequest $request, ConfigService $configService, $userId, $miscService)
+    public function __construct($appName, IRequest $request, ConfigService $configService, $miscService)
     {
         parent::__construct($appName, $request);
         $this->configService = $configService;
-        $this->userId = $userId;
         $this->miscService = $miscService;
     }
-    
-    
+
+    public static function generateOrcidUrl()
+    {
+        $appUri = \OC::$WEBROOT . '/index.php/apps/orcid/OrcidCode.php';
+        if (\OCP\App::isEnabled('files_sharding')) {
+            $redirectURL = \OCA\FilesSharding\Lib::getMasterURL() . $appUri;
+        } else {
+            $redirectURL = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . '://' . $_SERVER['SERVER_NAME'] . $appUri;
+        }
+        
+        return $redirectURL;
+    }
+
     /**
      * @NoCSRFRequired
      */
     public function OrcidCode()
     {
-        $this->miscService->log("ORCID_CODE !!");
+        $code = $_GET['code'];
+        $this->miscService->log('Received code: ' . $code);
+        
+        $user = \OCP\User::getUser();
+        
+        $clientAppID = trim($this->configService->getAppValue('clientAppID'));
+        $clientSecret = trim($this->configService->getAppValue('clientSecret'));
+        
+        $redirectURL = self::generateOrcidUrl();
+        
+        $content = "client_id=" . $clientAppID . "&" . "client_secret=" . $clientSecret . "&" . "grant_type=authorization_code&" . "code=" . $code . "&" . "redirect_uri=" . $redirectURL;
+        $this->miscService->log('$$$$ ' . $content);
+        $url = "https://orcid.org/oauth/token";
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($curl, CURLOPT_UNRESTRICTED_AUTH, TRUE);
+        
+        $json_response = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        if ($status === 0 || $status >= 300 || $json_response === null || $json_response === false) {
+            $this->miscService->log('ERROR: bad ws response. ' . $json_response);
+            return false;
+            // OCP\JSON::error();
+        } else {
+            $response = json_decode($json_response, true);
+        }
+        
+        $this->miscService->log('Got token: ' . serialize($response));
+        
+        if (! empty($response) && ! empty($response['orcid'])) {
+            $this->configService->setUserValue('orcid', $response['orcid']);
+            $this->configService->setUserValue('access_token', $response['access_token']);            
+            return new TemplateResponse($this->appName, 'thanks', [], 'blank');
+            
+        } else {
+            return false;
+        }
         
         return true;
     }
 }
 
-// \OCP\Util::writeLog('user_orcid','Received code: '.serialize($_GET), \OC_Log::WARN);
-
-// $code = $_GET['code'];
-
-// $user = \OCP\User::getUser();
-
-// $clientAppID  = OC_Appconfig::getValue('user_orcid', 'clientAppID');
-// $clientSecret = OC_Appconfig::getValue('user_orcid', 'clientSecret');
-
-// $appUri = \OC::$WEBROOT . 'apps/user_orcid/receive_orcid.php';
-// if(\OCP\App::isEnabled('files_sharding')){
-// 	$redirectURL = \OCA\FilesSharding\Lib::getMasterURL().$appUri;
-// }
-// else{
-// 	$redirectURL = (empty($_SERVER['HTTPS'])?'http':'https') . '://' . $_SERVER['SERVER_NAME'] .
-// 	$appUri;
-// }
-
-// $content = "client_id=".$clientAppID."&".
-// 		"client_secret=".$clientSecret."&".
-// 		"grant_type=authorization_code&".
-// 		"code=".$code."&".
-// 		"redirect_uri=".$redirectURL;
-
-// $url = "https://orcid.org/oauth/token";
-// $curl = curl_init($url);
-// curl_setopt($curl, CURLOPT_HEADER, false);
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($curl, CURLOPT_POST, true);
-// curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-// curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
-// curl_setopt($curl, CURLOPT_UNRESTRICTED_AUTH, TRUE);
-
-// $json_response = curl_exec($curl);
-// $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-// curl_close($curl);
-// if($status===0 || $status>=300 || $json_response===null || $json_response===false){
-// 	\OCP\Util::writeLog('files_sharding', 'ERROR: bad ws response. '.$json_response, \OC_Log::ERROR);
-// 	OCP\JSON::error();
-// }
-// else{
-// 	$response = json_decode($json_response, true);
-// }
-
-// \OCP\Util::writeLog('user_orcid','Got token: '.serialize($response), \OC_Log::WARN);
-
-// if(!empty($response) && !empty($response['orcid'])){
-// 	\OCP\Config::setUserValue($user, 'user_orcid', 'orcid', $response['orcid']);
-// 	\OCP\Config::setUserValue($user, 'user_orcid', 'access_token', $response['access_token']);
-// 	$tmpl = new OCP\Template("user_orcid", "thanks");
-// 	echo $tmpl->fetchPage();
-// }
-// else{
-// 	OCP\JSON::error();
-// }
